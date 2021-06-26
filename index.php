5d91e27bd68388f9d2d8125922d996e93f48221b
phamgiang01 <pgisme07@gmail.com>
2021-06-26 16:06:00 +0700

    Update file frontend

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Document</title>
+    
+</head>
+<body class="home index">
+    <div class="wrap">
+        <nav class="nav nav-custom">
+            <h1>Login</h1>
+            <form action="">
+                <input type="text" placeholder="Username/Email" name="user_uid">
+                <input type="password" placeholder="Password" name="user_pwd" >
+                <button type="submit">Login</button>
+            </form>
+            <button><a href="signup.php" class="btn btn-red signup">Register</a></button>
+        </nav>
+        <section>
+            <h3>Home</h3>
+        </section>
+        <footer>
+            <p>Design by <a href="#">Tran Toan</a></p>
+        </footer>
+    </div>
+    
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Document</title>
+    
+</head>
+<body class="home index">
+    <div class="wrap">
+        <nav class="nav nav-custom">
+            <h1>Login</h1>
+            <form action="">
+                <input type="text" placeholder="Username/Email" name="user_uid">
+                <input type="password" placeholder="Password" name="user_pwd" >
+                <button type="submit">Login</button>
+            </form>
+            <a href="signup.php" class="btn btn-red signup">Register</a>
+        </nav>
+        <section>
+            
+            <h3>Sign up</h3>
+            <div class="content">
+                <form action="">
+                    <input type="text" placeholder="First Name" name="user_first">
+                    <input type="text" placeholder="Last Name" name="user_last">
+                    <input type="email" placeholder="Email" name="user_email">
+                    <input type="text" placeholder="Username" name="user_uid">
+                    <input type="password" placeholder="Password" name="user_pwd">
+                    <button type="submit">Resgister</button>
+                </form>
+            </div>
+        </section>
+        <footer>
+            <p>Design by <a href="#">Tran Toan</a></p>
+        </footer>
+    </div>
+    
+</body>
+</html>
